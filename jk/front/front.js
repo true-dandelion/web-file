8bb8d3d34acc115756494ac36f9165d74ca0c433
true-dandelion <138918333+true-dandelion@users.noreply.github.com>
2025-07-30 11:47:54 +0800

    更新已经错误
    对静态路径显示

--- jk/front/front.js
+++ jk/front/front.js
@@ -3,9 +3,6 @@ const router = express.Router()
 const path = require('path')
 const { sessionMiddleware } = require('../middleware/middleware')
 
-// 配置静态文件访问
-router.use(express.static(path.join(__dirname, '../../public')))
-
 const authRedirectMiddleware = (req, res, next) => {
     if (req.user) {
         next()
@@ -13,6 +10,22 @@ const authRedirectMiddleware = (req, res, next) => {
         res.redirect('/login')
     }
 }
+
+//静态文件路由
+router.use('/ico', express.static(path.join(__dirname, '../../public/ico')))
+router.use('/login/assets', express.static(path.join(__dirname, '../../public/login/assets')))
+router.use('/file/assets', express.static(path.join(__dirname, '../../public/home/assets')))
+router.use('/setup/assets', express.static(path.join(__dirname, '../../public/set-up/assets')))
+router.use('/share/assets', express.static(path.join(__dirname, '../../public/share/assets')))
+
+//保护受限内容
+router.use('/file/*path', sessionMiddleware, authRedirectMiddleware)
+router.use('/setup/*path', sessionMiddleware, authRedirectMiddleware)
+
+//302跳转到/login
+router.get('/', (req, res) => {
+    res.redirect(302, '/login')
+})
 
 // 添加需要验证token的网页路由
 router.get('/File', sessionMiddleware, authRedirectMiddleware, (req, res) => {
@@ -25,6 +38,11 @@ router.get('/login', (req, res) => {
 
 router.get('/setup', sessionMiddleware, authRedirectMiddleware, (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/set-up/index.html'))
+})
+
+
+router.get('/vector/vector.js', (req, res) =>{
+res.sendFile(path.join(__dirname, '../../public/vector/vector.js'))
 })
 
 // 导出路由
